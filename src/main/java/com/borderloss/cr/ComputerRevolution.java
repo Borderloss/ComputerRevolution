33b4173915c222e25a39ac6725c9eab61d6cd6cf
Borderloss <cafrick2018@gmail.com>
2017-05-24 22:27:48 -0400

    initial commit

---
+++ src/main/java/com/borderloss/cr/ComputerRevolution.java
@@ -0,0 +1,28 @@
+package com.borderloss.cr;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "ComputerRevolution", name = "ComputerRevolution", version = "1.11.2-1.0")
+public class ComputerRevolution {
+
+    @Mod.Instance("ComputerRevolution")
+    public static ComputerRevolution instance;
+
+    @Mod.EventHandler
+    public void preInitilization(FMLPreInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void initilization(FMLInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void postInitilization(FMLPostInitializationEvent event){
+
+    }
+}
